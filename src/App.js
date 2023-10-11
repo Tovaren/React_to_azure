203df007c7dafd1df6aea6c2b87914ca716db7c5
Jakub Bouzek <jakub1999.jb@gmail.com>
2023-10-11 17:40:04 +0200

    bootstrap implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { saveAs } from 'file-saver';
-import { Button, Modal, Form as BSForm, Container, Row, Col } from 'react-bootstrap';
+import { Button, Modal, Form as BSForm, Container, Row, Col, Card} from 'react-bootstrap';
 
 function App() {
   const [text, setText] = useState("");
@@ -9,7 +9,7 @@ function App() {
   const [submittedData, setSubmittedData] = useState([]);
   const [showModal, setShowModal] = useState(false);
 
-  const options = ["Sign", "Instalace", "Odinstalace", "Výměna", "Předání"];
+  const options = ["Instalace softwaru", "Výměna hardwaru", "Nákup SW/HW", "Vyřešní ticketu", "Analýza problému"];
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -18,18 +18,16 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!text) {
-      alert('Prosím, vyplňte pole "Popis práce".');
+      alert('Prosím, vyplňte pole "Popis činnnosti".');
       return;
     }
     const date = new Date();
     const data = { text, selectedOption, file, date: date.toDateString(), time: date.getHours() };
     setSubmittedData([...submittedData, data]);
 
-    // Uložení dat do souboru txt pomocí file-saver
     const blob = new Blob([JSON.stringify(data)], {type: "text/plain;charset=utf-8"});
     saveAs(blob, "data.txt");
 
-    // Zavření modálního okna
     setShowModal(false);
   }
 
@@ -37,34 +35,33 @@ function App() {
     <Container>
       <Row className="justify-content-md-center">
         <Col md="auto">
-          {/* Zobrazení odeslaných dat */}
           {submittedData.map((data, index) => (
-            <div key={index}>
-              <p>Text: {data.text}</p>
-              <p>Vybraná možnost: {data.selectedOption}</p>
-              <p>Soubor: {data.file.name}</p>
-              <p>Datum a čas odeslání: {data.date}, {data.time} hodin</p>
-            </div>
+            <Card bg="light" text="dark" style={{ width: '18rem', marginTop: '10px' }} key={index}>
+              <Card.Header>{data.date} - {data.time} hodin</Card.Header>
+              <Card.Body>
+                <Card.Title>Popis činnosti: {data.text}</Card.Title>
+                <Card.Text>Činnost: {data.selectedOption}</Card.Text>
+                <Card.Text>Příloha: {data.file.name}</Card.Text>
+              </Card.Body>
+            </Card>
           ))}
 
-          {/* Tlačítko pro otevření modálního okna */}
-          <Button variant="primary" onClick={() => setShowModal(true)}>
-            Zadat nový výkaz
+          <Button variant="primary" onClick={() => setShowModal(true)} style={{ marginTop: '10px' }}>
+            Zadat výkaz
           </Button>
 
-          {/* Modální okno s formulářem */}
           <Modal show={showModal} onHide={() => setShowModal(false)}>
-            <Modal.Header closeButton = "Zavřít">
-              <Modal.Title>Výkaz práce</Modal.Title>
+            <Modal.Header closeButton>
+              <Modal.Title>Pracovní výkazy</Modal.Title>
             </Modal.Header>
             <Modal.Body>
               <BSForm onSubmit={handleSubmit}>
                 <BSForm.Group controlId="formText">
-                  <BSForm.Label>Popis provedené práce:</BSForm.Label>
-                  <BSForm.Control type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                  <BSForm.Label>Popis práce:</BSForm.Label>
+                  <BSForm.Control as="textarea" rows={3} value={text} onChange={(e) => setText(e.target.value)} />
                 </BSForm.Group>
                 <BSForm.Group controlId="formSelect">
-                  <BSForm.Label>Vyberte činnost:</BSForm.Label>
+                  <BSForm.Label>Vyberte možnost:</BSForm.Label>
                   <BSForm.Control as="select" value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
                     {options.map((option, index) => (
                       <option key={index} value={option}>{option}</option>
@@ -72,10 +69,10 @@ function App() {
                   </BSForm.Control>
                 </BSForm.Group>
                 <BSForm.Group controlId="formFile">
-                  <BSForm.Label>Přiložte přílohu:</BSForm.Label>
+                  <BSForm.Label>Přiložte soubor:</BSForm.Label>
                   <BSForm.Control type="file" onChange={handleFileChange} />
-                </BSForm.Group>
-                <Button variant="primary" type="submit">Odeslat</Button>
+                </BSForm.Group><br></br>
+                <Button variant="primary" type="Odeslat">Odeslat</Button>
               </BSForm>
             </Modal.Body>
           </Modal>
